b81d9512137653a13ea7d751bad5f678df6d9d31
tsevijpakpaki-boop <tsevijpakpaki@gmail.com>
2026-05-20 18:16:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+membres = []
+
 @app.route("/")
 def home():
-    return "Mon app Flask fonctionne sur Render !"
+    return render_template("index.html", membres=membres)
+
+@app.route("/ajouter", methods=["POST"])
+def ajouter():
+    nom = request.form.get("nom")
+    montant = request.form.get("montant")
+    if nom and montant:
+        membres.append({"nom": nom, "montant": montant})
+    return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
